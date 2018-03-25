529c80785920638b27fccf9355e04a77ffce49c8
Freeman <joe@freemansoft.com>
2018-03-25 11:40:06 +0000

    handle commented out lines lite vs AIY version had different contents

--- enable_uart_g_ether.sh
+++ enable_uart_g_ether.sh
@@ -10,30 +10,41 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-uart_enable='enable_uart=1'
-dwc2_enable='dtoverlay=dwc2'
+# config.txt changes
+uart_enable="enable_uart=1"
+dwc2_enable="dtoverlay=dwc2"
+# the lines normally has no leading whitespace
+uart_enabled="^"$uart_enable
+dwc2_enabled="^"$dwc2_enable
+
+# possible cmdline.txt changes
 load_serial="modules-load=dwc2,g_serial"
 load_ether="modules-load=dwc2,g_ether"
+# files
 boot_config_file="/boot/config.txt"
 boot_cmd_file="/boot/cmdline.txt"
 
-if grep -q "$uart_enable" $boot_config_file; then
+# first work on $boot_config_file
+# handles if the line is commented oiut
+if grep -q "$uart_enabled" $boot_config_file; then
     echo "uart previously enabled"
 else
-    grep -qF -- "$uart_enable" $boot_config_file || echo "$uart_enable" >> "$boot_config_file"
+    grep -qF -- "$uart_enabled" $boot_config_file || echo "$uart_enable" >> "$boot_config_file"
     echo "uart enabled"
 fi
-if grep -qF -- "$dwc2_enable" $boot_config_file; then 
+# handles if the line commented out
+if grep -q "$dwc2_enabled" $boot_config_file; then 
     echo "dtoverlay dwc2 previously enabled"
 else
-    grep -qF -- "$dwc2_enable" $boot_config_file || echo "$dwc2_enable" >> "$boot_config_file"
+    grep -qF -- "$dwc2_enabled" $boot_config_file || echo "$dwc2_enable" >> "$boot_config_file"
     echo "dtoverlay dwc2 enabled"
 fi
 
+# now work on $boot_cmd_file
 if grep -q "$load_ether" $boot_cmd_file; then 
     echo "ether gadget already added to cmdline"
 else
-    sed -i 's/rootwait/rootwait $load_ether/' $boot_cmd_file
+    sed -i "s/rootwait/rootwait $load_ether/" $boot_cmd_file
     echo "ether gadget appended cmdline"
 fi
 
