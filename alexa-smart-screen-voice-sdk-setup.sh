efd722c8108ee50958f3d5eb7c91c3a78f0b38e5
Joe Freeman <joe@freemansoft.com>
2020-12-12 17:40:04 -0500

    support enable/disable wake word installation

--- alexa-smart-screen-voice-sdk-setup.sh
+++ alexa-smart-screen-voice-sdk-setup.sh
@@ -5,6 +5,8 @@
 # Only installs the alexa voice sdk - as part of smart screen
 # Does not install the APL core library or the Smart Screen SDK!
 
+enable_wake_word=true
+
 if [ $(id -u) -eq 0 ]
 then
     echo "Please do not run as root"
@@ -46,30 +48,42 @@ pip install commentjson
 cd $HOME/sdk-folder/sdk-source    
 git clone --single-branch --branch v1.21.0 git://github.com/alexa/avs-device-sdk.git
 
-# This installs the sensory library to enable wake word.  
-# It pauses the installation asking for liscense agreement.
-# There should really be a switch here to enable/disable
-cd $HOME/sdk-folder/third-party
-git clone git://github.com/Sensory/alexa-rpi.git
-cd $HOME/sdk-folder/third-party/alexa-rpi/bin/
-./license.sh
+if [ "$enable_wake_word" = true ] ; then
+    # This installs the sensory library to enable wake word.  
+    # It pauses the installation asking for liscense agreement.
+    # There should really be a switch here to enable/disable
+    cd $HOME/sdk-folder/third-party
+    git clone git://github.com/Sensory/alexa-rpi.git
+    cd $HOME/sdk-folder/third-party/alexa-rpi/bin/
+    ./license.sh
 
-# Remove the -DSENSORY_KEY_WORD_DETECTOR.... to remove Sensory
-cd $HOME/sdk-folder/sdk-build
- cmake $HOME/sdk-folder/sdk-source/avs-device-sdk \
- -DSENSORY_KEY_WORD_DETECTOR=ON \
- -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$HOME/sdk-folder/third-party/alexa-rpi/lib/libsnsr.a \
- -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$HOME/sdk-folder/third-party/alexa-rpi/include \
- -DGSTREAMER_MEDIA_PLAYER=ON \
- -DPORTAUDIO=ON \
- -DPORTAUDIO_LIB_PATH=$HOME/sdk-folder/third-party/portaudio/lib/.libs/libportaudio.a \
- -DPORTAUDIO_INCLUDE_DIR=$HOME/sdk-folder/third-party/portaudio/include \
- -DCMAKE_BUILD_TYPE=DEBUG \
- -DCMAKE_INSTALL_PREFIX=$HOME/sdk-folder/sdk-install \
- -DRAPIDJSON_MEM_OPTIMIZATION=OFF
-
+    cd $HOME/sdk-folder/sdk-build
+    # Include the sensory library
+    cmake $HOME/sdk-folder/sdk-source/avs-device-sdk \
+        -DSENSORY_KEY_WORD_DETECTOR=ON \
+        -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$HOME/sdk-folder/third-party/alexa-rpi/lib/libsnsr.a \
+        -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$HOME/sdk-folder/third-party/alexa-rpi/include \
+        -DGSTREAMER_MEDIA_PLAYER=ON \
+        -DPORTAUDIO=ON \
+        -DPORTAUDIO_LIB_PATH=$HOME/sdk-folder/third-party/portaudio/lib/.libs/libportaudio.a \
+        -DPORTAUDIO_INCLUDE_DIR=$HOME/sdk-folder/third-party/portaudio/include \
+        -DCMAKE_BUILD_TYPE=DEBUG \
+        -DCMAKE_INSTALL_PREFIX=$HOME/sdk-folder/sdk-install \
+        -DRAPIDJSON_MEM_OPTIMIZATION=OFF
+else
+    cd $HOME/sdk-folder/sdk-build
+    cmake $HOME/sdk-folder/sdk-source/avs-device-sdk \
+        -DGSTREAMER_MEDIA_PLAYER=ON \
+        -DPORTAUDIO=ON \
+        -DPORTAUDIO_LIB_PATH=$HOME/sdk-folder/third-party/portaudio/lib/.libs/libportaudio.a \
+        -DPORTAUDIO_INCLUDE_DIR=$HOME/sdk-folder/third-party/portaudio/include \
+        -DCMAKE_BUILD_TYPE=DEBUG \
+        -DCMAKE_INSTALL_PREFIX=$HOME/sdk-folder/sdk-install \
+        -DRAPIDJSON_MEM_OPTIMIZATION=OFF
+fi
 make install
 
+# copy the config.json we got from amazon
 cp $HOME/config.json $HOME/sdk-folder/sdk-source/avs-device-sdk/tools/Install
 
 cd $HOME/sdk-folder/sdk-source/avs-device-sdk/tools/Install 
